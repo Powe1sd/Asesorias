434bdd46aa6a82eb3f430b89cdaba9e0ab1657e1
temssl <brayan324554@gmail.com>
2025-05-12 23:45:16 -0500

    first commit

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-// Verifica si la API de vibración está disponible
-const supportsVibration = "vibrate" in navigator;
+document.querySelectorAll(".vibrate-button").forEach((button) => {
+  button.addEventListener("click", function () {
+    button.classList.add("vibrate");
 
-// Vibración al hacer clic en botones
-const vibrateButtons = document.querySelectorAll(".vibrate-button");
-vibrateButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (supportsVibration) {
-      navigator.vibrate(100); // Vibrar durante 100ms
-    }
+    // Quitar la clase después de la animación
+    setTimeout(() => {
+      button.classList.remove("vibrate");
+    }, 300); // Duración de la animación en milisegundos
   });
 });
-
-// Selecciona el audio de bienvenida
-const welcomeAudio = document.getElementById("welcome-audio");
-
-// Mostrar el splash screen y reproducir el audio
-document.querySelector(".splash-screen").style.display = "block";
-welcomeAudio.play().catch((error) => {
-  console.log("El usuario debe interactuar con la página primero:", error);
-});
-
-// Ocultar el splash después de 3 segundos y mostrar el contenido
-setTimeout(() => {
-  document.querySelector(".splash-screen").style.display = "none";
-  document.querySelector(".content").style.display = "block";
-}, 3000);

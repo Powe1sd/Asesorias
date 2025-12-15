65257a1b7ec3036c248856e2c3fcfe6b52d1bb9c
temssl <brayan324554@gmail.com>
2025-12-14 23:55:11 -0500

    first commit

--- app.js
+++ app.js
@@ -17,3 +17,26 @@ window.addEventListener("load", () => {
         });
       });
     });
+
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+    const splashScreen = document.getElementById('splash');
+    const content = document.getElementById('content');
+
+    // Tiempo que dura la pantalla de carga (en milisegundos)
+    const splashDuration = 3000; 
+
+    setTimeout(() => {
+        // Oculta el splash con una transición de opacidad
+        splashScreen.style.opacity = '0'; 
+
+        setTimeout(() => {
+            // Una vez que la transición termina, oculta el splash completamente
+            splashScreen.style.display = 'none';
+            // Muestra el contenido principal
+            content.style.display = 'block'; 
+        }, 600); // Coincide con la duración de la transición en el CSS
+        
+    }, splashDuration);
+});
